922dc642d150bbfaf737635f799f8c5344fc3898
midnight <ilagsoun021@gmail.com>
2023-07-22 16:59:54 -0700

    betty

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -9,17 +9,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int i = 0;
-    char *p;
+	unsigned int i = 0;
+	char *p;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    p = malloc(nmemb * size);
-    if (p == NULL)
-        return (NULL);
-    for (i = 0; i < (nmemb * size); i++)
-        p[i] = '\0';
-    return (p);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
 }
 
 /**
@@ -32,41 +32,41 @@ void *_calloc(unsigned int nmemb, unsigned int size)
  **/
 void merge(int *arr, int *tmp, int start, int mid, int end)
 {
-    /* Sizes and temporary arrays */
-    int size_left = mid - start + 1, size_right = end - mid;
-    int *array_left, *array_right;
-    /* Counters */
-    int left, right, i = 0;
+	/* Sizes and temporary arrays */
+	int size_left = mid - start + 1, size_right = end - mid;
+	int *array_left, *array_right;
+	/* Counters */
+	int left, right, i = 0;
 
-    array_left = &tmp[0];
-    array_right = &tmp[size_right];
-    for (left = 0; left < size_left; left++)
-        array_left[left] = arr[start + left];
-    for (right = 0; right < size_right; right++)
-        array_right[right] = arr[mid + 1 + right];
-    left = 0, right = 0, i = start;
-    /* Merging temporary arrays back into the main array */
-    while (left < size_left && right < size_right)
-    {
-        if (array_left[left] <= array_right[right])
-            arr[i] = array_left[left], left++;
-        else
-            arr[i] = array_right[right], right++;
-        i++;
-    }
-    /* Merging the remaining left array back into the main array */
-    while (left < size_left)
-        arr[i] = array_left[left], left++, i++;
-    /* Merging the remaining right array back into the main array */
-    while (right < size_right)
-        arr[i] = array_right[right], right++, i++;
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(array_left, left);
-    printf("[right]: ");
-    print_array(array_right, right);
-    printf("[Done]: ");
-    print_array(&arr[start], left + right);
+	array_left = &tmp[0];
+	array_right = &tmp[size_right];
+	for (left = 0; left < size_left; left++)
+		array_left[left] = arr[start + left];
+	for (right = 0; right < size_right; right++)
+		array_right[right] = arr[mid + 1 + right];
+	left = 0, right = 0, i = start;
+	/* Merging temporary arrays back into the main array */
+	while (left < size_left && right < size_right)
+	{
+		if (array_left[left] <= array_right[right])
+			arr[i] = array_left[left], left++;
+		else
+			arr[i] = array_right[right], right++;
+		i++;
+	}
+	/* Merging the remaining left array back into the main array */
+	while (left < size_left)
+		arr[i] = array_left[left], left++, i++;
+	/* Merging the remaining right array back into the main array */
+	while (right < size_right)
+		arr[i] = array_right[right], right++, i++;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array_left, left);
+	printf("[right]: ");
+	print_array(array_right, right);
+	printf("[Done]: ");
+	print_array(&arr[start], left + right);
 }
 
 /**
@@ -80,17 +80,17 @@ void merge(int *arr, int *tmp, int start, int mid, int end)
  */
 void mergesort(int *array, int *tmp, int start, int end)
 {
-    int mid;
+	int mid;
 
-    mid = (start + end) / 2;
-    if ((start + end) % 2 == 0)
-        mid--;
-    if (mid >= start)
-    {
-        mergesort(array, tmp, start, mid);
-        mergesort(array, tmp, mid + 1, end);
-        merge(array, tmp, start, mid, end);
-    }
+	mid = (start + end) / 2;
+	if ((start + end) % 2 == 0)
+		mid--;
+	if (mid >= start)
+	{
+		mergesort(array, tmp, start, mid);
+		mergesort(array, tmp, mid + 1, end);
+		merge(array, tmp, start, mid, end);
+	}
 }
 
 /**
@@ -102,12 +102,12 @@ void mergesort(int *array, int *tmp, int start, int end)
  */
 void merge_sort(int *array, size_t size)
 {
-    int *tmp;
+	int *tmp;
 
-    if (!array || size < 2)
-        return;
-    tmp = _calloc(size, sizeof(int));
-    mergesort(array, tmp, 0, size - 1);
-    free(tmp);
+	if (!array || size < 2)
+		return;
+	tmp = _calloc(size, sizeof(int));
+	mergesort(array, tmp, 0, size - 1);
+	free(tmp);
 }
 
