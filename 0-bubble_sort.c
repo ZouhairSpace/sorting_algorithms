96b319fa04705e56eb62042ff648e135bd6332bd
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-07-21 18:18:30 -0700

    0x1B. C - Sorting algorithms & Big O_T0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts the elements of an array in ascending
+ * order using Bubble Sort
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
